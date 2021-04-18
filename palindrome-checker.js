721b32d9c88b39ba666134d62d0c9d4f0ec5ec15
Maynard <80284789+ManiacalMaynard@users.noreply.github.com>
2021-04-18 15:25:34 -0700

    Add files via upload
    updated to working html page

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,7 +1,11 @@
-function palindrome(str) {
+function palindrome() {
 
-    let thingOne = str.toLowerCase().replace(/[^a-z^0-9]/g, '');
-    let thingTwo = thingOne.split("").reverse().join("");
-    return thingOne == thingTwo ? true : false;
-  
-  };
+  string = document.getElementById("inputString").value;
+  console.log(string);
+  let thingOne = string.toLowerCase().replace(/[^a-z^0-9]/g, '');
+  let thingTwo = thingOne.split("").reverse().join("");
+  return thingOne == thingTwo ? 
+  document.getElementById('output').innerHTML = 'Congrats, you found a palindrome!' : 
+  document.getElementById('output').innerHTML = 'Oh no, it\'s not!';  
+
+};
