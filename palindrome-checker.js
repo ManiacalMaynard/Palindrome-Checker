0c4ac5a72ce9a28145c5a53f893ec982fd699f35
Maynard <80284789+ManiacalMaynard@users.noreply.github.com>
2021-04-18 11:58:21 -0700

    Update palindrome-checker.js

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -3,9 +3,5 @@ function palindrome(str) {
     let thingOne = str.toLowerCase().replace(/[^a-z^0-9]/g, '');
     let thingTwo = thingOne.split("").reverse().join("");
     return thingOne == thingTwo ? true : false;
-    
-  }
-
-
-
-palindrome("eye");
+  
+  };
