da03db27559f5a81f75c4d287dbda75b289bb1c0
Maynard <80284789+ManiacalMaynard@users.noreply.github.com>
2021-04-19 14:30:25 -0700

    Add files via upload

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,3 +1,34 @@
+//Thought it fun to put a random placeholder string into the form field.
+
+function randomPlaceholder() {
+  let copyStrings = [
+    'Go hang a salami, I\'m a lasagna hog!',
+    'Never odd or even.',
+    'Dogma I am God',
+    'Mr. Owl ate my metal worm.',
+    'Was it a car or a cat I saw?',
+    'Murder for a jar of red rum.',
+    'Rats live on no evil star.', 
+    'Live on time, emit no evil.', 
+    'Step on no pets.',
+    'Don\'t nod.',
+    'I did, did I?',
+    'My gym.',
+    'Red rum, sir, is murder.',
+    'Top spot.',
+    'Was it a cat I saw?',
+    'Eva, can I see bees in a cave?',
+    'No lemon, no melon.'
+  ];
+  let number = Math.floor(Math.random() * (copyStrings.length));
+  document.getElementById('inputString').placeholder = copyStrings[number];
+}
+window.onload = (event) => {
+  randomPlaceholder();
+}
+
+//The checker strips down the string and compares it to the reverse. Easy peasy. 
+
 function palindrome() {
 
   string = document.getElementById("inputString").value;
@@ -9,3 +40,5 @@ function palindrome() {
   document.getElementById('output').innerHTML = 'Oh no, it\'s not!';  
 
 };
+
+//That's it, thanks for looking at my code!
